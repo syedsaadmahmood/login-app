cdda21ff2c9171f83cc9f0bb46dcc1f28f4d5f2f
Saad Mahmood <saadmahmood1994@gmail.com>
2023-02-18 21:01:02 +0500

    Remove console log

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ const App = () => {
         setErrorMessage(data.message);
       }
     } catch (error) {
-      console.log('error', error)
       setErrorMessage('Something went wrong!');
     }
   };
