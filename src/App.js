31992ed3dc792117c7bf982dfbe9b87db4c1a3a0
Saad Mahmood <saadmahmood1994@gmail.com>
2023-02-18 20:05:24 +0500

    Project setup

---
+++ src/App.js
@@ -0,0 +1,73 @@
+
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [responseData, setResponseData] = useState()
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!username) {
+      setErrorMessage('Please enter a username.');
+      return;
+    }
+
+    if (!password) {
+      setErrorMessage('Please enter a password.');
+      return;
+    }
+    
+    const headers = {
+      'Xtag1': '11223344',
+      'Auth': `${username}:${password}`
+    };
+    try {
+      const response = await fetch('https://some-domain.tld/api/login', { headers });
+      const data = await response.json();
+      if (data.status === 'ok') {
+        setResponseData(data)
+      } else {
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      setErrorMessage('Something went wrong!');
+    }
+  };
+
+  if (responseData) {
+    return <LoggedInPage username={username} session={responseData.session} />;
+  }
+  
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+}
+
+const LoggedInPage = ({ username, session }) =>  {
+  return (
+    <div>
+      <h1>Logged In</h1>
+      <p>Hello {username}, your session is {session}.</p>
+    </div>
+  );
+}
+
+export default App;
